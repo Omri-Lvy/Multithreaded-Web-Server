1157cbe28bea387e7dce03d84ac1bc8bf1eda74a
Omri Levy <omri.lvy@gmail.com>
2024-02-11 15:42:09 +0200

    Final project code

--- src/main/java/exceptions/HttpProcessingException.java
+++ src/main/java/exceptions/HttpProcessingException.java
@@ -3,14 +3,8 @@ package exceptions;
 import enums.HttpStatusCode;
 
 public class HttpProcessingException extends Exception {
-    private final HttpStatusCode errorCode;
 
     public HttpProcessingException(HttpStatusCode errorCode) {
         super(errorCode.getMessage());
-        this.errorCode = errorCode;
-    }
-
-    public HttpStatusCode getErrorCode() {
-        return errorCode;
     }
 }
