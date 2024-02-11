c8b4214959cdf70c3b2ef6835c621f7f243fb35c
Omri Levy <omri.lvy@gmail.com>
2024-02-11 17:21:58 +0200

    fix compile.sh file

--- compile.sh
+++ compile.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-mvn compile
+# Set the source directory
+SOURCE_DIR="src/main/java"
+
+# Set the target directory for compiled classes
+TARGET_DIR="target/classes"
+
+# Compile all Java files, including those in subdirectories
+javac -d "$TARGET_DIR" -cp "$TARGET_DIR" "$SOURCE_DIR"/config/*.java "$SOURCE_DIR"/server/*.java "$SOURCE_DIR"/*.java
 
 # Check if the compilation was successful
 if [ $? -eq 0 ]; then
